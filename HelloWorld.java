8fca3ae555b312cf8f30241f76c81bc8c0276289
ultifinitus <programming.ultifinitus@gmail.com>
2014-05-20 21:42:25 -0600

    Added a couple strings to run in a for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld{
 	public static void main(String[] args){
+	
+		String strs = new String[3];
+		strs[0]="Well This is Interesting";
+		strs[1]="yeah... it is";
+		strs[2]="Welp, while we're here, let's have some juice";
+	
+		for (String G: strs){
+			System.out.println(G);
+		}
+		
 		System.out.println("Herro Werld");
 	}
 }
