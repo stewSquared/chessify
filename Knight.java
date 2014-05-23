5834e8babbcdf99e996118da607dcf99e2e93004
Stewart Stewart <stewinsalot@gmail.com>
2014-05-23 14:50:41 -0600

    Bishop and Knight functional.

--- Knight.java
+++ Knight.java
@@ -1,10 +1,12 @@
 public class Knight extends Piece {
+
     public String toString() {
 	return "N";
     }
 
-    public boolean legalMove(Point move) {
+    protected boolean legalMove(Point delta) {
 	return Math.abs(move.x) + Math.abs(move.y) == 3 &&
-	    Math.abs(move.x) == 1 ^ Math.abs(move.y) == 1;
+	    ((Math.abs(move.x) == 1) ^ (Math.abs(move.y) == 1)) &&
+	    !delta.equals(new Point(0,0));
     }
 }
