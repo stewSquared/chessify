0f66f30bb5dffc0b2ed9aeabc5562f6660db9f70
Stewart Stewart <stewinsalot@gmail.com>
2014-05-23 14:49:45 -0600

    Fixed up abstract Piece class.

--- Piece.java
+++ Piece.java
@@ -2,11 +2,24 @@ public abstract class Piece {
 
     private Point pos;
 
+    protected abstract boolean legalMove(Point delta);
+
+    public abstract String toString();
+
     public Piece(Point pos) {
 	this.pos = pos;
     }
 	
     public Point getPosition() {
 	return pos;
+    }
+
+    public Boolean move(Point delta) {
+	if (this.legalMove(delta)) {
+	    pos.translate(delta.x, delta.y);
+	    return True;
+	} else {
+	    return False;
+	}
     }
 }
