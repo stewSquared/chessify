84b4017591290a3db268b1a130ce91549d7e4654
Stewart Stewart <stewinsalot@gmail.com>
2014-06-04 15:32:06 -0600

    fixed pawns being able to move two spaces forward after first move

--- Pawn.java
+++ Pawn.java
@@ -2,28 +2,40 @@ import java.awt.Point;
 
 public class Pawn extends Piece {
 
+    private boolean moved;
+    
     public Pawn(Point inpos, String inteam){
-		super(inpos,inteam);
-	}
-
+	super(inpos,inteam);
+	this.moved = false;
+    }
+    
     public String toString() {
-		return ("P");
+	return ("P");
     }
 
+    public void move(Point delta, ChessBoard b) {
+	super.move(delta, b);
+	this.moved = true;
+    }
+    
     public boolean legalMove(Point delta, ChessBoard b) {
 	
-	boolean badDirection =
-	    (team == "white" && delta.y >= 0)
-	      || (team == "black" && delta.y <= 0);
-
-	if (badDirection) {System.out.println(delta); return false;}
-	    
-	Point target = new Point(pos.x + delta.x,
-				 pos.y + delta.y);
-
-	return (delta.x == 0) ? // straight move?
-	    b.isEmpty(target) && pathFree(delta, b) :
-	    (Math.abs(delta.x) == 1 && Math.abs(delta.y) == 1) ? // diagonal move?
-	    !b.isEmpty(target) : false;
+        boolean badDirection =
+            (team == "white" && delta.y >= 0)
+	    || (team == "black" && delta.y <= 0);
+	
+        if (badDirection) {System.out.println(delta); return false;}
+        
+        Point target = new Point(pos.x + delta.x,
+                                 pos.y + delta.y);
+	
+	if (delta.x == 0) { // straight?
+            return b.isEmpty(target) && pathFree(delta, b)
+		&& !((Math.abs(delta.y) == 2) && moved);
+	} else if (Math.abs(delta.x) == 1 && Math.abs(delta.y) == 1) { // diagonal?
+            return !b.isEmpty(target);
+	} else {
+	    return false;
+	}
     }
 }
